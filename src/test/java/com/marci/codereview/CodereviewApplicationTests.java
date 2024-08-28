ccbcecc92b847597fd6987bf778e7d54ab04a2cd
Marcelino Parado <106438094+marcelino013@users.noreply.github.com>
2024-08-28 23:28:10 +0800

    initial upload

---
+++ src/test/java/com/marci/codereview/CodereviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marci.codereview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodereviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
